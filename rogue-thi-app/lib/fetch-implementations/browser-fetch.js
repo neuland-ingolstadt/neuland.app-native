8700fa7a70a5f9f1d1c5f91ec0689656ddd57b1b
Robert Eggl <github@roberteggl.dev>
2023-07-06 20:04:12 +0200

    Moves backend to TypeScript

--- rogue-thi-app/lib/fetch-implementations/browser-fetch.js
+++
@@ -1,9 +0,0 @@
-/**
- * A fetch implementation that uses the actual `fetch` method.
- * This wrapper is necessary for compatibility with other implementations.
- */
-export default class BrowserFetchConnection {
-  async fetch(url, options) {
-    return fetch(url, options)
-  }
-}
