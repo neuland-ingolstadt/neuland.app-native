5ff65cdacff2c3b2ec299265111fb65be9270eeb
Robert Eggl <github@roberteggl.dev>
2023-11-02 17:23:42 +0100

    fixes map floor picker bug (#35)

--- app.config.ts
+++ app.config.ts
@@ -35,7 +35,7 @@ module.exports = {
         android: {
             package: 'app.neuland',
             userInterfaceStyle: 'automatic',
-            versionCode: 30,
+            versionCode: 31,
         },
         extra: {
             eas: {
