5c5707cea2dc62352474cad629f290c331c5ab26
Robert Eggl <github@roberteggl.dev>
2023-11-08 23:57:17 +0100

    adjust tabs content inset behavior (#47)

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -69,6 +69,7 @@ const styles = StyleSheet.create({
         width: '94%',
         alignSelf: 'center',
         paddingTop: 5,
+        paddingBottom: 45,
     },
     heading: {
         fontSize: 32,
