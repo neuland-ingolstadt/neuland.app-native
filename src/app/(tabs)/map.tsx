c821db3f4a397a37cae2c55d92c007d8a9b2d237
Robert Eggl <github@roberteggl.dev>
2023-11-11 22:58:59 +0100

    Add floor substitutes to MapScreen

--- src/app/(tabs)/map.tsx
+++ src/app/(tabs)/map.tsx
@@ -62,6 +62,10 @@ export const MapScreen = (): JSX.Element => {
     const FLOOR_SUBSTITUTES: Record<string, string> = {
         0: 'EG',
         0.5: '1.5',
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
     }
 
     enum LoadingState {
