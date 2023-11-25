d06adcb068077ecc12c3bce54f34b1bdd0e14ef0
M4tse <mm903-1@bingo-ev.de>
2023-11-25 16:20:31 +0100

    Added mobility functionality

--- src/app/(pages)/mobility.tsx
+++ src/app/(pages)/mobility.tsx
@@ -1,9 +1,18 @@
 import NeulandAPI from '@/api/neuland-api'
 import MobilityRow from '@/components/Elements/Pages/MobilityRow'
 import Divider from '@/components/Elements/Universal/Divider'
+import Dropdown from '@/components/Elements/Universal/Dropdown'
+import ToggleRow from '@/components/Elements/Universal/ToggleRow'
 import { type Colors } from '@/stores/colors'
+import MobilityJSON from '@/stores/data/mobility.json'
 import { MobilityContext } from '@/stores/provider'
-import { type CLEvents } from '@/stores/types/neuland-api'
+import {
+    type Bus,
+    type CLEvents,
+    type Charging,
+    type Parking,
+    type Train,
+} from '@/stores/types/neuland-api'
 import { useTheme } from '@react-navigation/native'
 import React, { useEffect, useState } from 'react'
 import {
@@ -26,9 +35,25 @@ export default function Mobility(): JSX.Element {
     }
     const [error, setError] = useState<Error | null>(null)
     const [loadingState, setLoadingState] = useState(LoadingState.LOADING)
-    const { mobilityKind, mobilityStation } = React.useContext(MobilityContext)
+    const {
+        mobilityKind,
+        mobilityStation,
+        toggleMobilityStation,
+        toggleMobilityKind,
+    } = React.useContext(MobilityContext)
+
+    const displayTypes = ['Bus', 'Train', 'Car', 'EV']
+
+    const busLocations = MobilityJSON.bus.stations.map(
+        (station) => station.name
+    )
+    const trainLocations = MobilityJSON.train.stations.map(
+        (station) => station.name
+    )
+    const defaultBusLocation = MobilityJSON.bus.defaultStation
+    const defaultTrainLocation = MobilityJSON.train.defaultStation
     useEffect(() => {
-        void loadEvents()
+        void loadEvents(mobilityKind, getID(mobilityStation, mobilityKind))
             .then(() => {
                 setLoadingState(LoadingState.LOADED)
             })
@@ -37,11 +62,59 @@ export default function Mobility(): JSX.Element {
                 setLoadingState(LoadingState.ERROR)
             })
     }, [])
+    function getID(name: string, kind: string): string {
+        let id
+        if (kind === 'Bus') {
+            id = MobilityJSON.bus.stations.find((x) => x.name === name)?.id
+            if (id === undefined) {
+                return defaultBusLocation
+            }
+        } else {
+            id = MobilityJSON.train.stations.find((x) => x.name === name)?.id
+            if (id === undefined) {
+                return defaultTrainLocation
+            }
+        }
 
-    async function loadEvents(): Promise<void> {
-        const mobilityData = (await NeulandAPI.getBusPlan(
-            mobilityStation
-        )) as CLEvents[]
+        return id
+    }
+    function getName(id: string, kind: string = mobilityKind): string {
+        let name: string | undefined
+        if (kind === 'Bus') {
+            name = MobilityJSON.bus.stations.find((x) => x.id === id)?.name
+            if (name === undefined) {
+                return getName(defaultBusLocation, 'Bus')
+            }
+        } else {
+            name = MobilityJSON.train.stations.find((x) => x.id === id)?.name
+            if (name === undefined) {
+                return getName(defaultTrainLocation, 'Train')
+            }
+        }
+        return name
+    }
+    async function loadEvents(kind: string, station: string): Promise<void> {
+        let mobilityData: any[] = []
+        console.log('Debug: ' + kind + station)
+        switch (kind.toLowerCase()) {
+            case 'bus':
+                mobilityData = (await NeulandAPI.getBusPlan(station)) as Bus[]
+                break
+            case 'train':
+                mobilityData = (await NeulandAPI.getTrainPlan(
+                    station
+                )) as Train[]
+                break
+            case 'car':
+                mobilityData = (await NeulandAPI.getParkingData()) as Parking[]
+                break
+            case 'ev':
+                mobilityData =
+                    (await NeulandAPI.getCharingStationData()) as Charging[]
+                break
+            default:
+                break
+        }
 
         const newEvents = mobilityData.map((x) => ({
             ...x,
@@ -50,9 +123,10 @@ export default function Mobility(): JSX.Element {
         }))
         setEvents(newEvents)
     }
-
-    const onRefresh: () => void = () => {
-        void loadEvents()
+    function onRefresh(kind: string, station: string): void {
+        setLoadingState(LoadingState.LOADING)
+        console.log('Station: ' + station)
+        void loadEvents(kind, getID(station, kind))
             .then(() => {
                 setLoadingState(LoadingState.LOADED)
             })
@@ -61,60 +135,116 @@ export default function Mobility(): JSX.Element {
                 setLoadingState(LoadingState.ERROR)
             })
     }
-
     return (
         <ScrollView
             refreshControl={
-                loadingState !== LoadingState.LOADING ? (
+                loadingState !== LoadingState.LOADING &&
+                loadingState !== LoadingState.LOADED ? (
                     <RefreshControl
                         refreshing={loadingState === LoadingState.REFRESHING}
-                        onRefresh={onRefresh}
+                        onRefresh={() => {
+                            onRefresh(mobilityKind, mobilityStation)
+                        }}
                     />
                 ) : undefined
             }
         >
-            {loadingState === LoadingState.LOADING && (
-                <View style={styles.loadingContainer}>
-                    <ActivityIndicator size="small" color={colors.primary} />
+            <ToggleRow
+                items={displayTypes}
+                selectedElement={mobilityKind}
+                setSelectedElement={(x) => {
+                    toggleMobilityKind(x)
+                    onRefresh(x, mobilityStation)
+                }}
+            />
+
+            {(mobilityKind === 'Bus' || mobilityKind === 'Train') && (
+                <View style={[styles.dropDownView]}>
+                    <Dropdown
+                        fontSize={20}
+                        data={
+                            mobilityKind === 'Bus'
+                                ? busLocations
+                                : trainLocations
+                        }
+                        defaultValue={getName(mobilityStation)}
+                        defaultText={getName(mobilityStation)}
+                        onSelect={(x) => {
+                            toggleMobilityStation(x)
+                            onRefresh(mobilityKind, x)
+                        }}
+                        selected={getName(getID(mobilityStation, mobilityKind))}
+                    />
                 </View>
             )}
-            {loadingState === LoadingState.ERROR && (
-                <View>
-                    <Text style={[styles.errorMessage, { color: colors.text }]}>
-                        {error?.message}
-                    </Text>
-                    <Text style={[styles.errorInfo, { color: colors.text }]}>
-                        An error occurred while loading the data.{'\n'}Pull down
-                        to refresh.
-                    </Text>
-                </View>
-            )}
-            {loadingState === LoadingState.LOADED && (
-                <>
-                    <View
-                        style={[
-                            styles.loadedContainer,
-                            { backgroundColor: colors.card },
-                        ]}
-                    >
-                        {events.map((event, index) => (
-                            <React.Fragment key={index}>
-                                <MobilityRow
-                                    item={event}
-                                    colors={colors}
-                                    detailed={true}
-                                    kind={mobilityKind}
-                                />
-                                {index !== events.length - 1 && (
-                                    <Divider
-                                        color={colors.labelTertiaryColor}
-                                    />
-                                )}
-                            </React.Fragment>
-                        ))}
+            <ScrollView
+                refreshControl={
+                    loadingState !== LoadingState.LOADING ? (
+                        <RefreshControl
+                            refreshing={
+                                loadingState === LoadingState.REFRESHING
+                            }
+                            onRefresh={() => {
+                                onRefresh(mobilityKind, mobilityStation)
+                            }}
+                        />
+                    ) : undefined
+                }
+            >
+                {loadingState === LoadingState.LOADING && (
+                    <View style={styles.loadingContainer}>
+                        <ActivityIndicator
+                            size="small"
+                            color={colors.primary}
+                        />
                     </View>
-                </>
-            )}
+                )}
+                {loadingState === LoadingState.ERROR && (
+                    <View>
+                        <Text
+                            style={[
+                                styles.errorMessage,
+                                { color: colors.text },
+                            ]}
+                        >
+                            {error?.message}
+                        </Text>
+                        <Text
+                            style={[styles.errorInfo, { color: colors.text }]}
+                        >
+                            An error occurred while loading the data.{'\n'}Pull
+                            down to refresh.
+                        </Text>
+                    </View>
+                )}
+                {loadingState === LoadingState.LOADED && (
+                    <>
+                        <View
+                            style={[
+                                styles.loadedContainer,
+                                { backgroundColor: colors.card },
+                            ]}
+                        >
+                            {events.map((event, index) => (
+                                <React.Fragment key={index}>
+                                    <MobilityRow
+                                        item={event}
+                                        colors={colors}
+                                        detailed={true}
+                                        kind={mobilityKind.toLowerCase()}
+                                    />
+                                    {index !== events.length - 1 && (
+                                        <Divider
+                                            color={colors.labelTertiaryColor}
+                                            width={'90%'}
+                                        />
+                                    )}
+                                </React.Fragment>
+                            ))}
+                        </View>
+                    </>
+                )}
+            </ScrollView>
         </ScrollView>
     )
 }
@@ -143,5 +273,9 @@ const styles = StyleSheet.create({
         marginTop: 14,
         marginBottom: 24,
         justifyContent: 'center',
+    },
+    dropDownView: {
+        fontSize: 14,
+        height: 50,
     },
 })
