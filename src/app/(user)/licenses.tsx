ab41152dbbb9ec50851db3f278382d8458c68883
Robert Eggl <github@roberteggl.dev>
2023-12-05 21:07:11 +0100

    Add licenses-static.json file and combine licenses with licenses-static

--- src/app/(user)/licenses.tsx
+++ src/app/(user)/licenses.tsx
@@ -1,12 +1,13 @@
 import FormList from '@/components/Elements/Universal/FormList'
 import { chevronIcon } from '@/components/Elements/Universal/Icon'
+import licensesStatic from '@/data/licenses-static.json'
 import licenses from '@/data/licenses.json'
 import { type FormListSections } from '@/types/components'
 import { MODAL_BOTTOM_MARGIN, PAGE_PADDING } from '@/utils/style-utils'
 import { useRouter } from 'expo-router'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { Platform, ScrollView, StyleSheet, View } from 'react-native'
 
 export default function Licenses(): JSX.Element {
     const router = useRouter()
@@ -15,27 +16,35 @@ export default function Licenses(): JSX.Element {
     const numberRegex = /\d+(\.\d+)*/
     const atRegex = /(?:@)/gi
 
-    const licensesList = Object.entries(licenses).map(([key, value]) => {
-        const version = key.match(numberRegex)
-        const nameWithoutVersion = key
-            .replace(atRegex, '')
-            .replace(version != null ? version[0] : '', '')
+    const licensesStaticFiltered = Object.entries(licensesStatic)
+        .filter(([key, license]) => license.platform.includes(Platform.OS))
+        .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {})
 
-        return {
-            title: nameWithoutVersion,
-            icon: chevronIcon,
-            onPress: async () => {
-                router.push('(user)/license')
-                router.setParams({
-                    license: value.licenses,
-                    version: version != null ? version[0] : '',
-                    licenseUrl: value.licenseUrl,
-                    repository: value.repository,
-                    name: nameWithoutVersion,
-                })
-            },
+    const licensesCombined = { ...licenses, ...licensesStaticFiltered }
+
+    const licensesList = Object.entries(licensesCombined).map(
+        ([key, value]) => {
+            const version = key.match(numberRegex)
+            const nameWithoutVersion = key
+                .replace(atRegex, '')
+                .replace(version != null ? version[0] : '', '')
+
+            return {
+                title: nameWithoutVersion,
+                icon: chevronIcon,
+                onPress: async () => {
+                    router.push('(user)/license')
+                    router.setParams({
+                        license: value.licenses,
+                        version: version != null ? version[0] : '',
+                        licenseUrl: value.licenseUrl,
+                        repository: value.repository,
+                        name: nameWithoutVersion,
+                    })
+                },
+            }
         }
-    })
+    )
 
     const sections: FormListSections[] = [
         {
