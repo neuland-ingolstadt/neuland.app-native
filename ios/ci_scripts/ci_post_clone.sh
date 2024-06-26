729c074ae07bf2385ac50b2eff83df634afa65e0
Robert Eggl <github@roberteggl.dev>
2024-06-26 17:55:12 +0200

    ...

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -5,14 +5,11 @@ export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
 echo "===== Installing Node.js ====="
 brew install node@20
-echo "===== Installing bun ====="
-brew tap oven-sh/bun
-brew install bun
 
 # Install dependencies
 echo "===== Running bun install ====="
 cd ..
-bun install
+npm install
 echo "===== Running pod install ====="
 cd ios
 pod install
