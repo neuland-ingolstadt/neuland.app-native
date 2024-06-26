5825f5a0e3e0b0a75f0937d1a175da3452408371
Robert Eggl <github@roberteggl.dev>
2024-06-26 17:16:44 +0200

    chore: Add script for post-clone setup

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+echo "===== Installling CocoaPods ====="
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods
+echo "===== Installing Node.js ====="
+brew install node@21
+echo "===== Installing bun ====="
+brew install bun
+
+# Install dependencies
+echo "===== Running yarn install ====="
+bun install
+echo "===== Running pod install ====="
+cd ios
+pod install
