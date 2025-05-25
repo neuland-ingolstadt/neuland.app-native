8b86d4823b128703d6397e50102a12b442b1bfef
Alex <54153428+alexpietsch@users.noreply.github.com>
2025-05-25 02:18:04 +0200

    feat: enhance event navigation and handling in cards (#202)

--- src/app/(screens)/cl-events.tsx
+++ src/app/(screens)/cl-events.tsx
@@ -11,17 +11,26 @@ import {
 import { pausedToast } from '@/utils/ui-utils'
 import { trackEvent } from '@aptabase/react-native'
 import { useQueries } from '@tanstack/react-query'
-import { useLocalSearchParams } from 'expo-router'
+import { router, useFocusEffect, useLocalSearchParams } from 'expo-router'
 import type React from 'react'
-import { Suspense, useEffect, useRef, useState } from 'react'
+import { Suspense, useCallback, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Animated, View, useWindowDimensions } from 'react-native'
+import {
+	Animated,
+	InteractionManager,
+	View,
+	useWindowDimensions
+} from 'react-native'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 
 export default function Events(): React.JSX.Element {
 	const { t } = useTranslation('common')
 	const { styles } = useStyles(stylesheet)
-	const { tab } = useLocalSearchParams<{ tab?: string }>()
+	const { tab, openEvent, id } = useLocalSearchParams<{
+		tab?: string
+		openEvent?: string
+		id?: string
+	}>()
 	const results = useQueries({
 		queries: [
 			{
@@ -91,6 +100,27 @@ export default function Events(): React.JSX.Element {
 			</View>
 		)
 	}
+
+	useFocusEffect(
+		useCallback(() => {
+			if (openEvent === 'true' && id) {
+				InteractionManager.runAfterInteractions(() => {
+					router.setParams({ openEvent: 'false' })
+					if (selectedData === 1) {
+						router.navigate({
+							pathname: '/events/sports/[id]',
+							params: { id }
+						})
+					} else {
+						router.navigate({
+							pathname: '/events/cl/[id]',
+							params: { id }
+						})
+					}
+				})
+			}
+		}, [openEvent, id, selectedData])
+	)
 
 	return (
 		<View style={styles.page}>
