511f4e2a9860c1d346831161b6cda25375e477ef
Robert Eggl <robert@eggl.dev>
2025-06-08 03:01:04 +0200

    feat: add showSplashScreen preference and integrate into splash behavior

--- src/app/(screens)/theme.tsx
+++ src/app/(screens)/theme.tsx
@@ -5,11 +5,18 @@ import { createStyleSheet, useStyles } from 'react-native-unistyles'
 import MultiSectionRadio from '@/components/Food/FoodLanguageSection'
 import ThemePreview from '@/components/Theme/ThemePreview'
 import SectionView from '@/components/Universal/SectionsView'
+import SingleSectionPicker from '@/components/Universal/SingleSectionPicker'
 import { usePreferencesStore } from '@/hooks/usePreferencesStore'
 
 export default function Theme(): React.JSX.Element {
 	const theme = usePreferencesStore((state) => state.theme)
 	const setTheme = usePreferencesStore((state) => state.setTheme)
+	const showSplashScreen = usePreferencesStore(
+		(state) => state.showSplashScreen
+	)
+	const setShowSplashScreen = usePreferencesStore(
+		(state) => state.setShowSplashScreen
+	)
 	const { t } = useTranslation(['settings', 'timetable'])
 	const { styles } = useStyles(stylesheet)
 
@@ -30,12 +37,6 @@ export default function Theme(): React.JSX.Element {
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.preview}>
-				<Text style={styles.previewLabel}>
-					{t('timetable:preferences.preview')}
-				</Text>
-				<ThemePreview theme={theme ?? 'auto'} onThemeChange={setTheme} />
-			</View>
 			<SectionView
 				title={t('theme.themes.title')}
 				footer={t('theme.themes.footer')}
@@ -46,6 +47,23 @@ export default function Theme(): React.JSX.Element {
 					action={setTheme as (item: string) => void}
 				/>
 			</SectionView>
+			<SectionView
+				title={t('settings:theme.splash.title')}
+				footer={t('settings:theme.splash.footer')}
+			>
+				<SingleSectionPicker
+					title={t('settings:theme.splash.showSplash')}
+					selectedItem={showSplashScreen}
+					action={setShowSplashScreen}
+					state={showSplashScreen}
+				/>
+			</SectionView>
+			<View style={styles.preview}>
+				<Text style={styles.previewLabel}>
+					{t('timetable:preferences.preview')}
+				</Text>
+				<ThemePreview theme={theme ?? 'auto'} onThemeChange={setTheme} />
+			</View>
 		</View>
 	)
 }
@@ -57,7 +75,7 @@ const stylesheet = createStyleSheet((theme) => ({
 	},
 	preview: {
 		marginHorizontal: theme.margins.page,
-		marginBottom: 12
+		marginTop: 42
 	},
 	previewLabel: {
 		color: theme.colors.labelSecondaryColor,
