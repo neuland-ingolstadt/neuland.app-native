f464e6eac95e732979f0fd9afa9e15fa5cd61b13
Robert Eggl <robert@eggl.dev>
2025-06-11 16:53:05 +0200

    refactor(PulsingDot): update style prop type and enhance animation logic

--- src/components/Universal/PulsingDot.tsx
+++ src/components/Universal/PulsingDot.tsx
@@ -1,7 +1,7 @@
 import type React from 'react'
-import { useEffect } from 'react'
-import type { ViewStyle } from 'react-native'
+import type { StyleProp, ViewStyle } from 'react-native'
 import Animated, {
+	useAnimatedReaction,
 	useAnimatedStyle,
 	useSharedValue,
 	withRepeat,
@@ -9,7 +9,7 @@ import Animated, {
 } from 'react-native-reanimated'
 
 interface PulsingDotProps {
-	style?: ViewStyle | ViewStyle[]
+	style?: StyleProp<ViewStyle>
 	duration?: number
 	minOpacity?: number
 	maxOpacity?: number
@@ -17,19 +17,26 @@ interface PulsingDotProps {
 
 const PulsingDot: React.FC<PulsingDotProps> = ({
 	style,
-	duration = 1000,
+	duration = 1200,
 	minOpacity = 0.3,
 	maxOpacity = 1
 }) => {
 	const pulseOpacity = useSharedValue(minOpacity)
+	const isActive = useSharedValue(true)
 
-	useEffect(() => {
-		pulseOpacity.value = withRepeat(
-			withTiming(maxOpacity, { duration }),
-			-1,
-			true
-		)
-	}, [duration, maxOpacity, pulseOpacity])
+	useAnimatedReaction(
+		() => isActive.value,
+		(active) => {
+			if (active) {
+				pulseOpacity.value = withRepeat(
+					withTiming(maxOpacity, { duration }),
+					-1,
+					true
+				)
+			}
+		},
+		[duration, maxOpacity]
+	)
 
 	const animatedStyle = useAnimatedStyle(() => ({
 		opacity: pulseOpacity.value
