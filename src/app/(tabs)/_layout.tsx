215235f1deabada0da27b04d55fb30a09fc94d71
Robert Eggl <robert@eggl.dev>
2025-07-08 17:40:42 +0200

    fix(layout): update storage migration to use MMKV for legacy data handling

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -7,7 +7,7 @@ import type React from 'react'
 import { use, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Platform } from 'react-native'
-import { useMMKVBoolean } from 'react-native-mmkv'
+import { MMKV, useMMKVBoolean } from 'react-native-mmkv'
 import { UserKindContext } from '@/components/contexts'
 import TabLayout from '@/components/Layout/Tabbar'
 import changelog from '@/data/changelog.json'
@@ -18,7 +18,7 @@ import { usePreferencesStore } from '@/hooks/usePreferencesStore'
 import { useSessionStore } from '@/hooks/useSessionStore'
 import { convertToMajorMinorPatch } from '@/utils/app-utils'
 import { humanLocations } from '@/utils/food-utils'
-import { appStorage, storage } from '@/utils/storage'
+import { storage } from '@/utils/storage'
 import { appIcons } from '../(screens)/app-icon'
 
 export default function HomeLayout(): React.JSX.Element {
@@ -50,15 +50,16 @@ export default function HomeLayout(): React.JSX.Element {
 	const updatedVersion = useFlowStore((state) => state.updatedVersion)
 	const [isOnboardedV1] = useMMKVBoolean('isOnboardedv1')
 	const [analyticsV1] = useMMKVBoolean('analytics')
+	const legacyStorage = new MMKV()
 	const oldAllergens = storage.getString('selectedUserAllergens')
 	// migration of old settings
 	if (isOnboardedV1 === true) {
 		setOnboarded()
-		appStorage.delete('isOnboardedv1')
+		legacyStorage.delete('isOnboardedv1')
 	}
 	if (analyticsV1 === true) {
 		setAnalyticsAllowed(true)
-		appStorage.delete('analytics')
+		legacyStorage.delete('analytics')
 	}
 
 	if (oldAllergens != null) {
