50193f9a0426a77362ab5bc196f26b8e00dac4a5
Robert Eggl <robert@eggl.dev>
2026-07-03 00:29:14 -0400

    refactor: adjust layout of InfoBox components in employee and student info sections for improved responsiveness

--- src/components/Settings/student-info-section.tsx
+++ src/components/Settings/student-info-section.tsx
@@ -21,54 +21,57 @@ export default function StudentInfoSection({
 	return (
 		<>
 			<View className="flex-row gap-2.5 mb-2.5">
-				<InfoBox
-					title={t('infoBoxes.gradesAndSubjects')}
-					value={ects !== undefined ? `${ects} ECTS` : '-'}
-					icon={{
-						ios: 'chart.xyaxis.line',
-						android: 'bar_chart',
-						web: 'ChartColumnIncreasing'
-					}}
-					href={'/grades' as RelativePathString}
-					style={{ flex: 5 }}
-				/>
-				<InfoBox
-					title={t('infoBoxes.lecturers')}
-					value={personalLecturersCount?.toString() ?? '-'}
-					icon={{
-						ios: 'person.2',
-						android: 'group',
-						web: 'Users'
-					}}
-					href={'/lecturers' as RelativePathString}
-					style={{ flex: 3 }}
-				/>
+				<View className="flex-[5]">
+					<InfoBox
+						title={t('infoBoxes.gradesAndSubjects')}
+						value={ects !== undefined ? `${ects} ECTS` : '-'}
+						icon={{
+							ios: 'chart.xyaxis.line',
+							android: 'bar_chart',
+							web: 'ChartColumnIncreasing'
+						}}
+						href={'/grades' as RelativePathString}
+					/>
+				</View>
+				<View className="flex-[3]">
+					<InfoBox
+						title={t('infoBoxes.lecturers')}
+						value={personalLecturersCount?.toString() ?? '-'}
+						icon={{
+							ios: 'person.2',
+							android: 'group',
+							web: 'Users'
+						}}
+						href={'/lecturers' as RelativePathString}
+					/>
+				</View>
 			</View>
 			<View className="flex-row gap-2.5 mb-2.5">
-				<InfoBox
-					title={t('infoBoxes.printerBalance')}
-					value={printerBalance ?? '-'}
-					icon={{
-						ios: 'printer',
-						android: 'print',
-						web: 'Printer'
-					}}
-					href={printLink as RelativePathString}
-					isExternalLink
-					style={{ flex: 3 }}
-				/>
-
-				<InfoBox
-					value={t('infoBoxes.library')}
-					title={t('infoBoxes.view')}
-					icon={{
-						ios: 'books.vertical',
-						android: 'book_5',
-						web: 'Library'
-					}}
-					href={'/library' as RelativePathString}
-					style={{ flex: 5 }}
-				/>
+				<View className="flex-[3]">
+					<InfoBox
+						title={t('infoBoxes.printerBalance')}
+						value={printerBalance ?? '-'}
+						icon={{
+							ios: 'printer',
+							android: 'print',
+							web: 'Printer'
+						}}
+						href={printLink as RelativePathString}
+						isExternalLink
+					/>
+				</View>
+				<View className="flex-[5]">
+					<InfoBox
+						value={t('infoBoxes.library')}
+						title={t('infoBoxes.view')}
+						icon={{
+							ios: 'books.vertical',
+							android: 'book_5',
+							web: 'Library'
+						}}
+						href={'/library' as RelativePathString}
+					/>
+				</View>
 			</View>
 		</>
 	)
